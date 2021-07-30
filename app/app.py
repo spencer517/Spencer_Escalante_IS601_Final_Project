944faee90918234d95a172f37ed2ea6ef9e8f62f
spenc <spencer517@msn.com>
2021-07-29 21:11:33 -0400

    Added: index.html Updated:app.py Note:hello world tutorial is now working with templates

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, Markup
+from flask import Flask, Markup, render_template
 
 # Create Flask's `app` object
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def hello():
-    return Markup("<h1>Hello World!</h1>")
+    return render_template("index.html", title='index')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
